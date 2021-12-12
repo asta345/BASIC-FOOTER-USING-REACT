d6d72af2fe4ec32c581f845710c6a03805c0a289
asta345 <81383112+asta345@users.noreply.github.com>
2021-12-12 17:02:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import "./App.css";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="page-container">
+      <div className="content-wrap">
+        <h1>CONTENT FOR FOOTER</h1>
+        <h1>CONTENT FOR FOOTER</h1>
+        <h1>CONTENT FOR FOOTER</h1>
+        <h1>CONTENT FOR FOOTER</h1>
+        <h1>CONTENT FOR FOOTER</h1>
+        <h1>CONTENT FOR FOOTER</h1>
+        <h1>CONTENT FOR FOOTER</h1>
+        <h1>CONTENT FOR FOOTER</h1>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
+
